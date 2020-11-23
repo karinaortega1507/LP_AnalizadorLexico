7aaaaa50e054169b0057e40a57d639b1e49eae53
karinaortega1507 <krortega@espol.edu.ec>
2020-11-23 15:34:47 -0500

    Se realizan las reglas sintácticas básicas

--- sintactico.py
+++ sintactico.py
@@ -1,51 +1,87 @@
+# se importa el módulo yacc
 import ply.yacc as yacc
 
+# se importa nuestra lista de tokens
 from lexico import tokens
 
-def p_expression_plus(p):
-  'expression : expression PLUS term'
-  p[0] = p[1] + p[3]
- 
-def p_expression_minus(p):
-  'expression : expression MINUS term'
-  p[0] = p[1] - p[3]
- 
-def p_expression_term(p):
-     'expression : term'
-     p[0] = p[1]
- 
-def p_term_times(p):
-     'term : term TIMES factor'
-     p[0] = p[1] * p[3]
- 
-def p_term_div(p):
-  'term : term DIVIDE factor'
-  p[0] = p[1] / p[3]
- 
-def p_term_factor(p):
-     'term : factor'
-     p[0] = p[1]
- 
-def p_factor_num(p):
-     'factor : NUMBER'
-     p[0] = p[1]
- 
-def p_factor_expr(p):
-     'factor : LPAREN expression RPAREN'
-     p[0] = p[2]
- 
- # Error rule for syntax errors
+
+# se define una línea de código
+def p_sentencia(p):
+    '''sentencia : expresion
+               |   instanciacion
+               |   declaracion'''
+    p[0] = p[1]
+
+
+def p_instanciacion(p):
+    '''instanciacion : SYMBOL ASIGN NUMBER
+                     | SYMBOL ASIGN TEXT
+                     | asignacion_estructuras'''
+    p[0] = 'instanciacion'
+
+
+def p_declaracion(p):
+    '''declaracion : CLASS SYMBOL
+                   | DEF SYMBOL'''
+    p[0] = 'declaracion'
+
+
+def p_expresion(p):
+    '''expresion : comentario
+                 | impresion_puts
+                 | expresion_valor'''
+    p[0] = p[1]
+
+
+def p_expresion_valor(p):
+    '''expresion_valor : NUMBER
+                       | TEXT'''
+    p[0] = p[1]
+
+
+def p_asignacion_estructuras(p):
+    '''asignacion_estructuras : SYMBOL ASIGN estructura_array'''
+    p[0] = 'asignacion_estructuras'
+
+
+def p_estructura_array(p):
+    '''estructura_array :  LBRACK valor_array RBRACK'''
+    p[0] = 'estructura_array'
+
+
+def p_valor_array(p):
+    '''valor_array : NUMBER
+                   | TEXT
+                   | TEXT COMMA valor_array
+                   | NUMBER COMMA valor_array'''
+
+
+
+def p_impresion_puts(p):
+    '''impresion_puts : PUTS TEXT
+                      | PUTS NUMBER
+                      | PUTS SYMBOL'''
+    p[0] = 'impresion_puts'
+
+
+def p_comentario(p):
+    '''comentario : COMMENT'''
+    p[0] = 'comentario'
+
+
+# Error rule for syntax errors
 def p_error(p):
-     print("Syntax error in input!")
- 
- # Build the parser
+    print("Syntax error in input!")
+
+
+# Build the parser
 parser = yacc.yacc()
- 
+
 while True:
-  try:
-    s = input('calc > ')
-  except EOFError:
+    try:
+        s = input('Ingrese su código >>> ')
+    except EOFError:
         break
-  if not s: continue
-  result = parser.parse(s)
-  print(result)
+    if not s: continue
+    result = parser.parse(s)
+    print(result)
